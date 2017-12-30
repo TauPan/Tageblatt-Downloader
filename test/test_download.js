c04c5020bca0abd623bc07d16260db5cb94181ca
Friedrich Delgado Friedrichs <friedel@nomaden.org>
2017-12-30 15:30:38 +0100

    scan download directory for most recent file date

--- test/test_download.js
+++ test/test_download.js
@@ -1,3 +1,5 @@
+import * as fs from "fs";
+
 import { Selector } from 'testcafe';
 
 import {me} from './roles';
@@ -5,8 +7,22 @@ import {me} from './roles';
 fixture `download page`
   .page `www.tageblatt.de/index.php?pageid=177`;
 
+
+function last_date() {
+  var maxDate = new Date('1970-01-01');
+  for (let v of fs.readdirSync(`${process.env.HOME}/Dropbox/tageblatt`)) {
+    let date = new Date(v.split(' ')[0]);
+    if (date > maxDate) {
+      maxDate = date;
+    }
+  }
+  return maxDate;
+};
+
 test( `download`, async t => {
+  const lastDate = last_date();
   await t
     .useRole(me)
-    .expect(Selector('#dateDropDown').visible).ok();
-});
+    .expect(Selector('#dateDropDown').visible).ok()
+    .click('#dateDropDown');
+  });
