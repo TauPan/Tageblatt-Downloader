239302d7e4853ecc2d659da2ec442eb97c68272c
Friedrich Delgado Friedrichs <friedel@nomaden.org>
2017-12-31 20:42:00 +0100

    factor out function to download issue for date

--- test/test_download.js
+++ test/test_download.js
@@ -2,11 +2,19 @@ const target_dir = `${process.env.HOME}/Dropbox/tageblatt`;
 const download_dir = `${process.env.HOME}/Downloads`;
 const wantedIssue = 'Buxtehuder Tageblatt';
 const epaper_page = `https://www.tageblatt.de/index.php?pageid=177`;
+
 import * as fs from "fs";
 
 import { Selector } from 'testcafe';
 
 import {me} from './roles';
+const yearOption = Selector('option.yearOption');
+const monthOption = Selector('option.monthOption');
+const dayOption = Selector('span.dayspan');
+const issueOption = Selector('option.editionOption')
+      .withText(wantedIssue);
+const downloadMode = Selector('button.downloadMode');
+const finalDate = Date.now();
 
 fixture `download page`
   .page(epaper_page);
@@ -40,17 +48,7 @@ async function change_selection(t, wanted_value, select, option) {
   return t;
 }
 
-test( `download`, async t => {
-  const lastFileDate = last_date();
-  const finalDate = Date.now();
-  const yearOption = Selector('option.yearOption');
-  const monthOption = Selector('option.monthOption');
-  const dayOption = Selector('span.dayspan');
-  const issueOption = Selector('option.editionOption')
-        .withText(wantedIssue);
-  const downloadMode = Selector('button.downloadMode');
-  let date = lastFileDate;
-  date.setDate(date.getDate() + 1);
+async function download_date_issue(t, date) {
   let wantedYear = date.getFullYear().toString();
   let currentYearOption = yearOption
       .withText(wantedYear);
@@ -59,12 +57,6 @@ test( `download`, async t => {
       .withText(wantedMonth);
   let currentDayOption = dayOption
       .withText(date.getDate().toString());
-  await t.useRole(me);
-  await t.navigateTo(epaper_page);
-  await t.expect(Selector('li#loginDropDown > a')
-                 .withText("MEINE DATEN").exists).ok();
-  await t
-    .expect(Selector('#dateDropDown').visible).ok();
   await t.click('#dateDropDown');
   await change_selection(t, wantedYear, Selector('select.yearSelect'),
                          currentYearOption);
@@ -73,19 +65,34 @@ test( `download`, async t => {
   await change_selection(t, wantedIssue,
                          Selector('select.editionSelect'), issueOption);
   await t
-    .expect(currentDayOption.visible).ok()
-    .click(currentDayOption);
-  await t
-    .expect(downloadMode.visible).ok()
-    .click(downloadMode)
-    .click('a#downloadComplete');
-  let downloadFilename = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} 12_00_00_${wantedIssue}.pdf`;
-  let downloadFile = `${download_dir}/${downloadFilename}`;
-  let targetFile = `${target_dir}/${downloadFilename}`;
-  await t
-    .expect(fs.exists(`${downloadFile}.crdownload`));
-  while (! fs.existsSync(downloadFile)) {
-    await t.wait(1000);
+    .expect(currentDayOption.visible).ok();
+    await t
+        .click(currentDayOption);
+      await t
+        .expect(downloadMode.visible).ok()
+        .click(downloadMode)
+        .click('a#downloadComplete');
+      let downloadFilename = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} 12_00_00_${wantedIssue}.pdf`;
+    let downloadFile = `${download_dir}/${downloadFilename}`;
+    let targetFile = `${target_dir}/${downloadFilename}`;
+    await t
+      .expect(fs.exists(`${downloadFile}.crdownload`));
+    while (! fs.existsSync(downloadFile)) {
+      await t.wait(1000);
+    }
   }
   fs.renameSync(downloadFile, targetFile);
+};
+
+test( `download`, async t => {
+  const lastFileDate = last_date();
+  let date = lastFileDate;
+  await t.useRole(me);
+  await t.navigateTo(epaper_page);
+  await t.expect(Selector('li#loginDropDown > a')
+                 .withText("MEINE DATEN").exists).ok();
+  await t
+    .expect(Selector('#dateDropDown').visible).ok();
+  date.setDate(date.getDate() + 1);
+  await download_date_issue(t, date);
 });
