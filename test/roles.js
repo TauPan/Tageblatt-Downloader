6c5e690c1a6b9805abba854d94e40eefb98d1195
Friedrich Delgado Friedrichs <friedel@nomaden.org>
2018-01-01 09:29:34 +0100

    wait until cookie "accept" button is present

--- test/roles.js
+++ test/roles.js
@@ -6,11 +6,13 @@ import {Role, Selector} from "testcafe";
 export const me = Role(
   'https://epaper.tageblatt.de/auth',
   async t => {
+    const cookie_button = Selector('div.cc_banner-wrapper a');
     const anmeldeknopf = Selector('a').withText("Anmelden");
     const userPass = JSON.parse(
       fs.readFileSync(userPassFile, 'utf-8'));
     await t
-      .click('div.cc_banner-wrapper a')
+      .expect(cookie_button.visible).ok()
+      .click(cookie_button)
       .typeText('div.Eingabetafel1 input[name="login"]',
                 userPass.user, {replace: true})
       .typeText('div.Eingabetafel1 input[name="pass"]',
